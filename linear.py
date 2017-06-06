0ea169502906ed234cc5fcfee50b2849262893c1
alaragebes <alaragb@gmail.com>
2017-06-06 16:52:10 +0300

    Added linear_search.

---
+++ linear.py
@@ -0,0 +1,20 @@
+def linear_search (myItem, myList):
+    found = False
+    position = 0
+    while position < len(myList) and not found :ß
+        if myList[position] == myItem :
+            found = True
+        position = position + 1
+        return found
+
+if __name__ == "__main__":
+    shopping = ["apples", "kiwi", "maple", "coconut"]
+    print "What item?"
+    item = raw_input(" ")
+    isitfound = linear_search (item, shopping)
+    if isitfound == True :
+        print ("Your item is here")
+    else :
+        print ("Nope, not here.")
+
+## couldn't find ERROR 
